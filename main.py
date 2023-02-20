1f321c57798bd801c566689beeb2fc686be36cb2
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-02-20 17:16:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,18 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['go'])
 def gen_keyboard(message):
     keyboard = InlineKeyboardMarkup(row_width=1)
-    keyboard.add(InlineKeyboardButton("Matchday", url="https://www.flashscore.ua/soccer/world/world-cup/#/2/8/zkyDYRLU/live"), InlineKeyboardButton("Group", url="https://www.flashscore.ua/soccer/world/world-cup/standings/#/2/8/zkyDYRLU/table"), InlineKeyboardButton("Bombardier", url="https://www.flashscore.ua/soccer/world/world-cup/standings/#/2/8/zkyDYRLU/top_scorers"), InlineKeyboardButton("Results", url="https://www.flashscore.ua/soccer/world/world-cup/results/"), InlineKeyboardButton("Matches", url="https://www.flashscore.ua/soccer/world/world-cup/fixtures/"))
+    keyboard.add(InlineKeyboardButton("Matchday", url="https://www.flashscore.ua/soccer/world/world-cup/#/2/8/zkyDYRLU/live"),
+                 InlineKeyboardButton("Group", url="https://www.flashscore.ua/soccer/world/world-cup/standings/#/2/8/zkyDYRLU/table"),
+                 InlineKeyboardButton("Bombardier", url="https://www.flashscore.ua/soccer/world/world-cup/standings/#/2/8/zkyDYRLU/top_scorers"),
+                 InlineKeyboardButton("Results", url="https://www.flashscore.ua/soccer/world/world-cup/results/"),
+                 InlineKeyboardButton("Matches", url="https://www.flashscore.ua/soccer/world/world-cup/fixtures/"))
     bot.send_message(message.chat.id, 'World Cup 2022', reply_markup=keyboard)
 
     
 @bot.message_handler(commands=['start'])
 def message_start(message):
-    bot.send_message(message.chat.id, 'Hello, user!')
-    
-def test():
-    pass
+    bot.send_message(f'Hello, {message.from_user.username}! \nNow i have next command:\n\n'f'/go')
 
-@bot.message_handler(func=lambda x: x.text.lower().startswith('python'))
-def message_text(message):
-    bot.send_message(message.chat.id, 'Python')
-
-    
 @app.route("/" + TOKEN, methods=["POST"])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
